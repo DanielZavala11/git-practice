822ad89cd9fce494197d9c00790b16d58f8c5ac8
DanielZavala11 <89770626+DanielZavala11@users.noreply.github.com>
2023-05-27 10:50:54 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <Routes>
+        <Route></Route>
+      </Routes>
+  )
+}
+
+export default App
